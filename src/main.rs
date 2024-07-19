a2a5d4105c34ce3d08a7fa943a0bbb8b4622c6cd
sohrab <azamighadim2@gmail.com>
2024-07-19 19:42:16 +0200

    hotfix: Added necessary (1 +2 = 3) to the program

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use reqwest::blocking::Client;
 use reqwest::Error;
 
 fn main() -> Result<(), Error> {
+
+    // add hotfix to the program
+    let (x,y) = (1,2);
+    println!("{}", format_args!("{x} + {y} = 3"));
+    
     let client = Client::new();
 
     let user_name = "tesuser".to_string();

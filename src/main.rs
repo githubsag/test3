a773227c1be59adea18fcfe2ec6b276a5c1b0533
sohrab <azamighadim2@gmail.com>
2024-07-19 17:20:45 +0200

    fixed Error: Err(reqwest::Error { kind: Builder, source: InvalidPort }), it was fake website

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Error> {
     let password: Option<String> = None;
 
     let response = client
-    .get("https://httpbin:org/")
+    .get("https://github.com/githubsag/test2/")
     .basic_auth(user_name, password)
     .send();
 
